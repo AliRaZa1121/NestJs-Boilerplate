54952aef8666310889d6ba685fd2d093ede730ba
Ali RaZa <aliraza.1121@yahoo.com>
2023-03-18 05:39:13 +0500

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DatabaseModule } from './config/database.config';
-import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './users/auth/auth.module';
-import { UserExistsRule } from './decorators/custom-validator';
-import { MailModule } from './mail/mail.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UsersModule} from './users/users.module';
+import {AuthModule} from './users/auth/auth.module';
+import {MailModule} from './mail/mail.module';
+import { ConfigurationsModule } from "./config/config.module";
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: '.env',
-    isGlobal: true,
-  }),
-  DatabaseModule,
+  imports: [
+  ConfigurationsModule,
   UsersModule,
   AuthModule,
   MailModule,
